6fb5aba7ff2fb8d763128566321a39b0e2667ac4
UmedaTakefumi <ut+github@azurestone.net>
2021-08-27 04:37:28 +0900

    edit scripts

--- bulkbuild_vagrant_boxes.sh
+++ bulkbuild_vagrant_boxes.sh
@@ -4,8 +4,8 @@ function bulkbuild_vagrant_boxes () {
 
   TEMPLATE_DIRS=$(git ls-files | sort | awk -F/ '{print $1}' | uniq | grep -v .git | grep -v .md | grep -v .sh)
   
-  for temp in ${TEMPLATE_DIRS[@]}; do
-    echo "## $temp" 
+  for target_template in ${TEMPLATE_DIRS[@]}; do
+    echo "## $target_template"
   done
 }
 
