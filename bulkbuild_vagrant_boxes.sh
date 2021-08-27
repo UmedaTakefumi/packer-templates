a28441bfa8b2287a8a0a2e21292ea3053949ec50
UmedaTakefumi <ut+github@azurestone.net>
2021-08-28 08:53:57 +0900

    edit script

--- bulkbuild_vagrant_boxes.sh
+++ bulkbuild_vagrant_boxes.sh
@@ -4,8 +4,15 @@ function bulkbuild_vagrant_boxes () {
 
   TEMPLATE_DIRS=$(git ls-files | sort | awk -F/ '{print $1}' | uniq | grep -v .git | grep -v .md | grep -v .sh)
   
-  for target_template in ${TEMPLATE_DIRS[@]}; do
-    echo "## $target_template"
+  for VAGRAND_BOX_NAME in ${TEMPLATE_DIRS[@]}; do
+    echo "## $VAGRAND_BOX_NAME"
+    cd $VAGRAND_BOX_NAME
+    packer build --only=virtualbox-iso template.json
+    vagrant box add $VAGRAND_BOX_NAME $VAGRAND_BOX_NAME-virtualbox.box
+    mkdir -p ~/Vgrant/$VAGRAND_BOX_NAME
+    cd ~/Vgrant/$VAGRAND_BOX_NAME
+    vagrant init $VAGRAND_BOX_NAME
+
   done
 }
 
