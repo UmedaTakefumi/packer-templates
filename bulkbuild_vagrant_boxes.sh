63e409fe5a8ae0e726f26e05461bc6e343053a1b
UmedaTakefumi <ut+github@azurestone.net>
2021-09-01 21:47:55 +0900

    fix

--- bulkbuild_vagrant_boxes.sh
+++ bulkbuild_vagrant_boxes.sh
@@ -112,7 +112,7 @@ function check_status_todolist () {
 
     
     if [ -f $VAGRAND_BOX_NAME/problem.md ]; then
-      echo "* problem"
+      echo "### problem"
       cat $VAGRAND_BOX_NAME/problem.md
     fi
     echo -e "\n"
